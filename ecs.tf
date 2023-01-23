387fc4fe2aaea9d35752eac566fff97de3c7ee9b
Harishankar4274 <harishankardubey2911@gmail.com>
2023-01-24 04:01:42 +0530

    ecr app_image updated

--- ecs.tf
+++ ecs.tf
@@ -2,13 +2,9 @@ resource "aws_ecs_cluster" "ecs-cluster" {
   name = var.aws_ecs_cluster_name
 }
 
-data "aws_ecr_repository" "demo_ecs_app" {
-  name = var.aws_ecr_repository
-}
-
 data "template_file" "demo_ecs_app" {
   vars = {
-    app_image      = data.aws_ecr_repository.demo_ecs_app.repository_url
+    app_image      = aws_ecr_repository.demo_ecs_app.repository_url
     app_port       = var.app_port
     fargate_cpu    = var.fargate_cpu
     fargate_memory = var.fargate_memory
