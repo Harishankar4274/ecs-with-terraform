3c7a7b8a9050ab957b93e1f9ff9b0a6027ebe978
Harishankar4274 <harishankardubey2911@gmail.com>
2023-01-24 07:04:25 +0530

    updated image

--- ecs.tf
+++ ecs.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_task_definition" "demo_ecs_app_def" {
   container_definitions = jsonencode([
     {
       name   = var.aws_ecr_repository
-      image  = var.aws_ecr_repository
+      image  = "600023384173.dkr.ecr.ap-south-1.amazonaws.com/demo_ecs_app:latest"
       cpu    = var.fargate_cpu
       memory = var.fargate_memory
       portMappings = [
